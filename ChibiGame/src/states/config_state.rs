85ec805ad764230f7dee82878af0be6a5344d1a8
SrMrBurchick <srmrburchick@gmail.com>
2023-10-11 01:29:47 +0300

    [ChibiGame] Implement config loading state
    Signed-off-by: SrMrBurchick <srmrburchick@gmail.com>

---
+++ ChibiGame/src/states/config_state.rs
@@ -0,0 +1,81 @@
+use async_std;
+use bevy::prelude::*;
+use crossbeam_channel::Receiver;
+
+use crate::components::{
+    common::events::{Event, Events, SystemEvents},
+    system::config::{wasm_load_config, Config},
+};
+
+#[derive(Resource, Deref)]
+pub struct ConfigReceiver(Receiver<String>);
+
+pub fn setup_config_loader(mut commands: Commands) {
+    let (async_tx, async_rx) = async_std::channel::bounded(10);
+    // Run async config reading
+    async_std::task::block_on(async move {
+        match wasm_load_config(async_tx).await {
+            _ => {
+                // Do nothing
+            }
+        }
+    });
+
+    let (sender, receiver) = crossbeam_channel::bounded::<String>(10);
+
+    // Wait for config readded
+    async_std::task::block_on(async move {
+        let sender_clone = sender.clone();
+        let result = async_rx.recv().await.unwrap();
+
+        sender_clone.send(result).unwrap();
+    });
+
+    // Add config receiver resource
+    commands.insert_resource(ConfigReceiver(receiver));
+}
+
+pub fn handle_config(
+    mut commands: Commands,
+    receiver: Res<ConfigReceiver>,
+    mut event_writer: EventWriter<Event>,
+) {
+    for result in receiver.try_iter() {
+        if result.is_empty() {
+            // Wait for valid config
+            continue;
+        }
+
+        let mut config = Config::new();
+        match config.load(result) {
+            Err(_) => {
+                event_writer.send(Event {
+                    event_type: Events::SystemEvents(SystemEvents::ConfigLoaded(false)),
+                });
+                continue;
+            },
+            _ => {
+                // Do nothing
+            }
+        }
+
+        match config.init_animations_map() {
+            Err(_) => {
+                event_writer.send(Event {
+                    event_type: Events::SystemEvents(SystemEvents::ConfigLoaded(false)),
+                });
+                continue;
+            },
+            _ => {
+                // Do nothing
+            }
+        }
+
+        // If everything ok
+        commands.spawn(config);
+
+        event_writer.send(Event {
+            event_type: Events::SystemEvents(SystemEvents::ConfigLoaded(true)),
+        });
+    }
+}
