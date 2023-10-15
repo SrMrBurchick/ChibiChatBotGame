73d70a910ca7fe8aea7e7e5b8c94cd4be286e02f
SrMrBurchick <srmrburchick@gmail.com>
2023-10-15 05:42:12 +0300

    [ChibiGame] Add actions strategies trait
    Signed-off-by: SrMrBurchick <srmrburchick@gmail.com>

---
+++ ChibiGame/src/components/gameplay/actions_strategies.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+use crate::components::{
+    movement::PlayerMovementComponent,
+    gameplay::gameplay_logic::GameplayLogicComponent,
+    common::events::Event
+};
+
+pub mod climb_strategy;
+
+#[reflect_trait]
+pub trait ActionLogicStrategy {
+    fn do_action(
+        &mut self,
+        movement: &PlayerMovementComponent,
+        gameplay: &GameplayLogicComponent,
+        event_writer: &mut EventWriter<Event>
+    );
+    fn is_valid_strategy(
+        &self,
+        gameplay: &GameplayLogicComponent
+    ) -> bool;
+    fn is_in_preparing_phase(&self) -> bool;
+}
---
+++ ChibiGame/src/components/gameplay/actions_strategies/climb_strategy.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+use crate::components::{
+    gameplay::{
+        gameplay_logic::GameplayLogicComponent,
+        actions_strategies::{
+            ActionLogicStrategy, ReflectActionLogicStrategy
+        }
+    },
+    movement::PlayerMovementComponent,
+    common::events::{
+        Event, Events, GameEvents
+    },
+    actions::Actions
+};
+
+#[derive(Component, Debug, Reflect)]
+#[reflect(ActionLogicStrategy)]
+pub struct ClimbStrategy{
+    preparing: bool
+}
+
+impl ClimbStrategy {
+    pub fn new() -> Self {
+        ClimbStrategy { preparing: false }
+    }
+}
+
+impl ActionLogicStrategy for ClimbStrategy {
+    fn do_action(
+        &mut self,
+        movement: &PlayerMovementComponent,
+        gameplay: &GameplayLogicComponent,
+        event_writer: &mut EventWriter<Event>
+    ) {
+        if gameplay.get_current_action() != Actions::Climb {
+            error!("Selected wrong strategy for action: {:?}", gameplay.get_current_action());
+            return;
+        }
+
+        if movement.can_climb {
+            if self.preparing {
+                self.preparing = false;
+                event_writer.send(
+                    Event {
+                        event_type: Events::GameEvents(GameEvents::ActionChanged(Actions::Climb))
+                    }
+                );
+            }
+        } else if !self.preparing {
+            self.preparing = true;
+            event_writer.send(
+                Event {
+                    event_type: Events::GameEvents(GameEvents::ActionChanged(Actions::Walk))
+                }
+            );
+        }
+    }
+    fn is_valid_strategy(
+        &self,
+        gameplay: &GameplayLogicComponent
+    ) -> bool {
+        gameplay.get_current_action() == Actions::Climb
+    }
+
+    fn is_in_preparing_phase(&self) -> bool {
+        self.preparing
+    }
+
+}
+
