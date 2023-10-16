767dd56b24827cd9cea2aeac6fd6f40d776c9494
SrMrBurchick <srmrburchick@gmail.com>
2023-10-17 02:42:02 +0300

    [Setup]: Add package script for Linux
    Signed-off-by: SrMrBurchick <srmrburchick@gmail.com>

---
+++ Package.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+## Init submodules
+git submodule update --init --recursive
+
+## Environment variables
+PROJECT_LOCATION="$PWD"
+WASM_SERVER_RUNNER="$PWD/tools/wasm-server-runner"
+GAME_CONFIGURATOR="$PWD/GameConfigurator"
+GAME="$PWD/ChibiGame"
+CHAT_BOT="$PWD/ChatBot"
+
+
+## Create ChibiChatBotGame directory
+PACKAGE_DIRECTORY="$PWD/ChibiChatBotGame"
+PACKAGE_TOOLS_DIRECTORY="$PWD/ChibiChatBotGame/Tools"
+if [ -d ${PACKAGE_DIRECTORY} ]; then
+    echo "Create ChibiChatBotGame package directory"
+    mkdir ${PACKAGE_DIRECTORY}
+    if [ -d ${PACKAGE_TOOLS_DIRECTORY} ]; then
+        mkdir ${PACKAGE_TOOLS_DIRECTORY}
+    fi
+fi
+
+########################## Setup wasm-server-runner ############################
+cd ${WASM_SERVER_RUNNER}
+cargo build --release
+
+### Go back
+cd ${PROJECT_LOCATION}
+
+### Copy binary to the package folder
+cp ${WASM_SERVER_RUNNER}/target/release/wasm-server-runner ${PACKAGE_TOOLS_DIRECTORY}
+
+######################### Build GameConfigurator ###############################
+cd ${GAME_CONFIGURATOR}
+### Generate makefile
+qmake6 -makefile
+
+### Build
+make
+
+### Copy GameConfigurator to package
+cp ${GAME_CONFIGURATOR}/build/release/GameConfigurator ${PACKAGE_DIRECTORY}
+
+######################### Build Game ###########################################
+cd ${GAME}
+
+### Build
+cargo build --target wasm32-unknown-unknown --release
+
+### Copy game to package
+cp ${GAME}/target/wasm32-unknown-unknown/release/ChibiGame.wasm ${PACKAGE_DIRECTORY}
+
+######################### Build Chat bot #######################################
+cd ${CHAT_BOT}
+
+## Build
+pyinstaller chat_bot.py
+
+## Copy chat bot to package
+cp ${CHAT_BOT}/build/chat_bot/chat_bot ${PACKAGE_DIRECTORY}
