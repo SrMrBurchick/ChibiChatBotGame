ff4d7ca1334135015158f6193531be13f976d09c
SrMrBurchick <srmrburchick@gmail.com>
2023-11-13 20:10:04 +0200

    [Setup] Update GameConfigurator deploy
    Signed-off-by: SrMrBurchick <srmrburchick@gmail.com>

--- tools/Scripts/Build/Win64/BuildGameConfigurator.bat
+++ tools/Scripts/Build/Win64/BuildGameConfigurator.bat
@@ -6,6 +6,7 @@ rem Init environments
 set GAME_CONFIGURATOR_LOCATION=%1
 set PACKAGE_LOCATION=%2
 set GAME_CONFIGURATOR=%GAME_CONFIGURATOR_LOCATION%\\build\\release\\GameConfigurator.exe
+set PACKAGED_CONFIGURATOR=%PACKAGE_LOCATION%\\GameConfigurator.exe
 
 rem Go to game location
 cd %GAME_CONFIGURATOR_LOCATION%
@@ -22,6 +23,6 @@ copy "%GAME_CONFIGURATOR%" "%PACKAGE_LOCATION%"
 
 rem Deploy configurator
 echo Deploy configurator
-windeployqt --release --qmldir %GAME_CONFIGURATOR_LOCATION%\\qml --libdir %PACKAGE_LOCATION%\\Libraries  --plugindir %PACKAGE_LOCATION%\\Plugins  %GAME_CONFIGURATOR%
+windeployqt --release --qmldir %GAME_CONFIGURATOR_LOCATION%\\qml --plugindir %PACKAGE_LOCATION%\\Plugins %PACKAGED_CONFIGURATOR%
 
 popd
